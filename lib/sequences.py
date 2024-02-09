fa12f21cb7e4f2d006a4bd6e384c188ea37fbc55
Elaine <elainedoud@gmail.com>
2024-02-08 21:05:33 -0500

    Just starting

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    thisList = []
+    for i in range(19, 0, 1):
+        num = i[i -1] + i[i]
+    
+
+#First we want to construct a list based on the Fibonnaci sequence
+# Fibonnaci sequence means it starts at zero, and then one element 
+# plus two element equals the third element, ect.
+# Lastly, we want to shorten the list to match the length 
+# provided
